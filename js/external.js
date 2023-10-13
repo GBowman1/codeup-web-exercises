280732b26b90e347e2c020aae16583d9e9748925
Garrett Bowman <garrettbowman248@gmail.com>
2023-10-13 13:46:02 -0500

    Feat: Completed exercise 2

---
+++ js/external.js
@@ -0,0 +1,8 @@
+'use strict';
+
+alert('Welcome to my Website!');
+// webpage greeting pop-up
+let userFavColor = prompt('What is your favorite color?');
+// Prompt to determine users favorite color and assign to variable userFavColor
+alert('Awesome, ' + userFavColor + ' is my favorite color as well!');
+// Pop-up to acknowledge favorite colors
