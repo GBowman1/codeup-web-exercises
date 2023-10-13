6369b0750ce8e744bb82febf4174966d37ecb444
Garrett Bowman <garrettbowman248@gmail.com>
2023-10-13 12:02:47 -0500

    Feat: Created notes for js w/ html

---
+++ js/js-notes.js
@@ -0,0 +1,10 @@
+"use strict"; // doesn't allow for tom foolery in the code
+
+// Alert - a pop up msg to the user from js
+alert("howdy from js")
+
+// confirm - a popup msg to user from js that can capture a boolean
+let confirmed = confirm('we have lunch at 12?')
+
+// prompt - a pop up msg to user from js that ask for a text value
+let userString = prompt('what is your favorite color?')
