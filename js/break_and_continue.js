e30bc7565ff7cf312c77521feb9570b27748f554
Garrett Bowman <garrettbowman248@gmail.com>
2023-10-19 13:32:43 -0500

    Feat: completed break and continue exercise

---
+++ js/break_and_continue.js
@@ -0,0 +1,38 @@
+'use strict';
+
+let userOdd = true;
+while (userOdd){
+    let userInput = parseFloat(prompt("Please enter your a odd number."));
+    if(userInput % 2 !== 0){
+        console.log(`Number to skip is: ${userInput}`)
+        for (let i = 1; i <= 50; i++) {
+            if (i % 2 === 0) {
+                continue;
+
+            } else if (i === userInput) {
+                console.log('Yikes! Skipping number: ' + i);
+                continue;
+            }
+
+            console.log('Here is a a odd number: ' + i);
+        }
+        break;
+
+    }
+}
+// quite possibly over complicated nested loop to allow a user to input value if not true loops prompt if
+// true continues to internal loop to consolelog message.
+
+
+
+// for (let i = 1; i <= 50; i++) {
+//     console.log(`Number to skip is: ${userInput}`)
+//     if (i % 2 !== 0) {
+//         // number isn't even
+//         // odd numbers aren't as cool
+//         // skip the rest of the loop and continue with the next iteration
+//         continue;
+//     }
+//
+//     console.log('Here is a a odd number: ' + i);
+// }
